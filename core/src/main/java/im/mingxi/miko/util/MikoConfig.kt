c4d79c90527fe9441d8beb1d15d09f3098c2acef
hbyswbq <w15926777737@gmail.com>
2026-05-08 00:07:52 +0800

    Update MikoConfig.kt
    Signed-off-by: hbyswbq <w15926777737@gmail.com>

--- core/src/main/java/im/mingxi/miko/util/MikoConfig.kt
+++ core/src/main/java/im/mingxi/miko/util/MikoConfig.kt
@@ -8,17 +8,29 @@ object MikoConfig {
 
     fun getCacheMap(): HashMap<String, String> {
         val map = HashMap<String, String>()
-        cache.allKeys()!!.forEach {
-            map[it] = cache.getString(it, "")!!
+        cache.allKeys()?.forEach {
+            map[it] = cache.getString(it, "") ?: ""
         }
         return map
     }
 
     fun getConfigMap(): HashMap<String, String> {
         val map = HashMap<String, String>()
-        config.allKeys()!!.forEach {
-            map[it] = cache.getString(it, "")!!
+        config.allKeys()?.forEach {
+            map[it] = config.getString(it, "") ?: ""
         }
         return map
     }
+
+    var autoTransferEnable: Boolean
+        get() = config.decodeBool("auto_transfer_enable", true)
+        set(value) = config.encode("auto_transfer_enable", value)
+
+    var autoTransferDelay: Int
+        get() = config.decodeInt("auto_transfer_delay", 0)
+        set(value) = config.encode("auto_transfer_delay", value)
+
+    var autoTransferReply: String
+        get() = config.decodeString("auto_transfer_reply", "已收款，谢谢") ?: "已收款，谢谢"
+        set(value) = config.encode("auto_transfer_reply", value)
 }
